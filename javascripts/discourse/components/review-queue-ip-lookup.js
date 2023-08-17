3bd1ee1fc21fa10302e611b171b9d8b54eb9cbdb
Faizaan Gagan <faizaan@discourse.org>
2023-08-17 18:05:44 +0530

    initial commit

---
+++ javascripts/discourse/components/review-queue-ip-lookup.js
@@ -0,0 +1,21 @@
+import { action } from "@ember/object";
+import IpLookup from "admin/components/ip-lookup";
+import AdminUser from "admin/models/admin-user";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+
+export default class ReviewQueueIpLookup extends IpLookup {
+  @action
+  async lookup() {
+    if(this.ip == "init") {
+      let userId = this.userId;
+      try {
+        let userInfo = await AdminUser.find(userId);
+        this.set("ip", userInfo.ip_address);
+      } catch(e) {
+        popupAjaxError(e);
+      }
+    }
+
+    super.lookup(...arguments);
+  }
+}
