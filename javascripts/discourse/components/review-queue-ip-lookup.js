04709f29b13aedc548da70dab9318148bcec978e
David Taylor <david@taylorhq.com>
2024-03-27 17:33:36 +0000

    DEV: Update linting

--- javascripts/discourse/components/review-queue-ip-lookup.js
+++ javascripts/discourse/components/review-queue-ip-lookup.js
@@ -1,17 +1,17 @@
 import { action } from "@ember/object";
+import { popupAjaxError } from "discourse/lib/ajax-error";
 import IpLookup from "admin/components/ip-lookup";
 import AdminUser from "admin/models/admin-user";
-import { popupAjaxError } from "discourse/lib/ajax-error";
 
 export default class ReviewQueueIpLookup extends IpLookup {
   @action
   async lookup() {
-    if(this.ip == "init") {
+    if (this.ip === "init") {
       let userId = this.userId;
       try {
         let userInfo = await AdminUser.find(userId);
         this.set("ip", userInfo.ip_address);
-      } catch(e) {
+      } catch (e) {
         popupAjaxError(e);
         return; // no need to go further in this case
       }
